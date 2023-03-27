a006a72ba87f5009bd66636091fb445625cbd23d
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-03-27 11:34:41 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.futuretaskdemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.futuretaskdemo to javafx.fxml;
+    exports com.example.futuretaskdemo;
+}
